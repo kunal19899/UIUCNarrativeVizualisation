80aa32bba57c61c531d88cbac5255adb0ea846ce
Kunal Samant <kunal19899@gmail.com>
2023-07-25 20:02:00 -0700

    adding dataset

---
+++ ageovertime.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function() {
+  // Load the CSV file using D3.js
+  d3.csv("/Users/kunalsamant/Documents/UIUC/Summer%202023/DataViz/UIUCNarrativeVizualisation/data1.csv")
+    .then(function(data) {
+      // Once the data is loaded, you can work with it here
+
+      // For demonstration purposes, let's log the data to the console
+      console.log(data);
+    })
+    .catch(function(error) {
+      // If there's an error loading the data, handle it here
+      console.error("Error loading data:", error);
+    });
+});
